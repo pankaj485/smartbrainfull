4f1c8c0448f9c168f0eb77add5900c6eec4ac36d
pankaj485 <pankaj485@users.noreply.github.com>
2021-03-31 21:59:47 +0530

    updating database

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const image = require('./controllers/image');
 const db = knex({
 	client: 'pg',
 	connection: {
-		host: '127.0.0.1',
+		host: 'postgresql-perpendicular-56429',
 		user: 'postgres',
 		password: 'pass',
 		database: 'smartbrain',
